c194afe67335820b204d511162e6b18fe338d699
Tor Soderquist <tor.soderquist@mjolnertech.com>
2017-09-20 20:44:17 -0400

    initial

---
+++ client/main.js
@@ -0,0 +1,16 @@
+// main.js
+
+function Greeting({ hello }) {
+    return <div>{ hello }</div>;
+}
+
+const GreetingArrow = ({ hello }) => (
+    <div>{ hello }</div>
+)
+
+class Component {
+    constructor(props) {
+        this.props = props;
+    }
+}
+
