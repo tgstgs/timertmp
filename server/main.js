2f7afcc859ae5d9309cf56bfb41f6d3675c34bd7
Tor Soderquist <tor.soderquist@mjolnertech.com>
2017-09-22 17:54:00 -0400

    before new branch

--- server/main.js
+++ server/main.js
@@ -1,11 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Random } from 
+//import { Random } from 
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Meteor.publish('time', function() {
+  //Meteor.publish('time', function() {
     // tbd
-  });
+  //});
 
   // Time = new Mongo.Collection('time'); // Try this later
 
